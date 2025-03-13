20751aa28ce78c92264359314a81c317c9a49442
Zanuarestu Ramadhani <rama.dev.zero@gmail.com>
2025-03-13 17:39:08 +0700

    feat: implement custom player

--- your_player.rb
+++ your_player.rb
@@ -1,15 +1,44 @@
 require './base_player.rb'
 
 class YourPlayer < BasePlayer
+  def initialize(game:, name:)
+    super(game:, name:)
+    @current_position = nil
+    @visited = {}
+  end
+
   def next_point(time:)
-    # Implement your strategy here.
-    {
-      row: 0,
-      col: 0
-    }
+    if @current_position.nil?
+      @current_position = { row: 0, col: 0 }
+      @visited[@current_position] = true
+      return @current_position
+    end
+
+    next_move = find_next_unvisited_point
+    @visited[@current_position] = true
+    @current_position = next_move
+    next_move
   end
 
   def grid
     game.grid
+  end
+
+  private
+
+  def find_next_unvisited_point
+    max_row = grid.max_row
+    max_col = grid.max_col
+  
+    (0..max_row).each do |row|
+      cols = row.even? ? (0..max_col) : (0..max_col).to_a.reverse
+      cols.each do |col|
+        point = { row: row, col: col }
+        return point if !@visited[point] && grid.is_valid_move?(from: @current_position, to: point)
+      end
+    end
+  
+    # If all points are visited, return the current position
+    @current_position
   end
 end
